289adf0a35040c84d910d61da02e92c42cd36a09
Piotr Stanczyk <pstanczyk@ilm.com>
2012-01-24 12:49:14 -0800

    what a mess 1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-#include <iostream>
-
-using namespace std;
-
-int 
-main(int argc, char ** argv)
-{
-    int i = 10;
-=======
 void
 function()
 {
     
 }
-
 
 int
 functionInt (int k)
@@ -21,11 +10,9 @@ functionInt (int k)
     return k*2;
 }
 
-
 }
 int 
 main (int argc, char ** argv)
 {
->>>>>>> 66466a81050871991a8739635907adff34f350cf
     return EXIT_SUCCESS;
 }
